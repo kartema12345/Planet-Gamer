f881a21bd52a0a390a2c850d53b5ae620707af6f
kartema12345 <kartema12345@gmail.com>
2025-11-18 20:42:33 -0300

    Delete config.php

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Configuration placeholders - update these values in production
-define('MP_ACCESS_TOKEN', 'COLOQUE_SEU_TOKEN_MERCADOPAGO_AQUI');
-define('SITE_NAME', 'Planet Gamer');
-define('ADMIN_EMAIL', 'you@example.com');
